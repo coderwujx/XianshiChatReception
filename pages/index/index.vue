e819dc8ca37294dab6afca0edb898d8a7d87944f
coderwujx <2689607189@qq.com>
2025-01-14 14:51:24 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="content">
+		<view class="top-bar">
+			<!-- 聊天页头部 -->
+			<view class="top-bar-left">
+
+			</view>
+			<view class="top-bar-conter">
+
+			</view>
+			<view class="top-bar-right">
+
+			</view>
+		</view>
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+	.content {}
+</style>
